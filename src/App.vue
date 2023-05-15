e47cb342409ec47c87414565dd3e7e365ec538cc
EduardRo <eduardstancu@yahoo.com>
2023-05-15 12:12:29 +0300

    save71

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
-<template>
-  <div><h1>Hello DC</h1></div>
-  <div>
-  <select v-model="componentName">
-    <option value="Home">Home</option>
-    <option value="About">About</option>
-  </select>
-  <keep-alive>
-    <component :is="componentName"></component>
-  </keep-alive>
- 
-</div>
-</template>
+<template></template>
 
 <script>
-import Home from './components/Home.vue'
-import About from './components/About.vue'
-
-export default{
-  name:'App',
-  data(){
-    return {
-      componentName:"Home",
-    }
-  },
-  components:{
-    About:About,
-    Home:Home,
-  }
-}
-
+export default {
+  name: "App",
+};
 </script>
+
+<style>
+</style>
